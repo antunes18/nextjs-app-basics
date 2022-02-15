a4bca81babb96b33c164f4ff26dff58b00b6e937
Thiago Antunes dos Santos <thiagoantunes0101@gmail.com>
2022-02-15 15:58:33 -0300

    Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,24 +1,37 @@
-import Link from "next/link";
+import Link from "next/link"
+import Head from "next/head"
+import Script from "next/script"
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Index page</a>
-        </Link>
-      </h2>
-      <h3>
-        <Link href="/posts/second-post">
-          <a>Second Post page</a>
-        </Link>
-      </h3>
-      <h4>
-        <a href="https://nextjs.org/" target="_blank">
-          Next.js website
-        </a>
-      </h4>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script 
+          src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"
+          strategy="lazyOnload"
+          onLoad={() => console.log(`Chart.js loaded correctly!`)}
+        ></Script>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Index page</a>
+          </Link>
+        </h2>
+        <h3>
+          <Link href="/posts/second-post">
+            <a>Second Post page</a>
+          </Link>
+        </h3>
+        <h4>
+          <a href="https://nextjs.org/" target="_blank">
+            Next.js website
+          </a>
+        </h4>
+      </Layout>
     </>
   );
 }
--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,26 +1,29 @@
-import Link from "next/link";
+import Link from "next/link"
+import Layout from "../../components/layout"
 
 export default function SecondPost() {
   return (
     <>
-      <h1>Second Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Index page</a>
-        </Link>
-      </h2>
-      <h3>
-        <Link href="/posts/first-post">
-          <a className="foo" target="_blank">
-            First Post page
+      <Layout>
+        <h1>Second Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Index page</a>
+          </Link>
+        </h2>
+        <h3>
+          <Link href="/posts/first-post">
+            <a className="foo" target="_blank">
+              First Post page
+            </a>
+          </Link>
+        </h3>
+        <h4>
+          <a href="https://nextjs.org/" target="_blank">
+            Next.js website
           </a>
-        </Link>
-      </h3>
-      <h4>
-        <a href="https://nextjs.org/" target="_blank">
-          Next.js website
-        </a>
-      </h4>
+        </h4>
+      </Layout>
     </>
   );
 }
